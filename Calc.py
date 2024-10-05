1e6c2732d4a9d99dca6c6bf253203c924465d5e1
winter <winterahm@gmail.com>
2024-10-05 14:24:28 +0900

    곱셈 기능 추가

--- Calc.py
+++ Calc.py
@@ -3,6 +3,7 @@ def main():
     num2 = int(input("두 번째 숫자를 입력하세요: "))
     print(f"합계: {num1 + num2}")
     print(f"차이: {num1 - num2}")
+    print(f"곱셈: {num1 * num2}")
 
 if __name__ == "__main__":
     main()
